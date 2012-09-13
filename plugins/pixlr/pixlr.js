ec4a464bbbb4d56aaedffbb1a374a5fdcb4824c9
uxcandy_boomyjee <debian5-64.test>
2012-09-14 00:51:27 +0200

    Plugin skeleton

---
+++ plugins/pixlr/pixlr.js
@@ -0,0 +1,20 @@
+dayside.plugins.push(function(){
+    dayside.editor.bind("codeTabCreated",function (b,tab){
+        var file = tab.options.file;
+        var ext = file.split(".").pop();
+        if (ext=='jpg' || ext=='png' || ext=="jpeg") {
+            tab.element.append(
+                teacss.jQuery("<a href='#'>")
+                .css({
+                    position: 'absolute',
+                    bottom: 15, left: 60
+                })
+                .html("Open Pixlr")
+                .click(function(e){
+                    e.preventDefault();
+                    pixlr.overlay.show({image:file, title:file.split("/").pop()});
+                })
+            )
+        }
+    });
+});
