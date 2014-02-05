aec720c6f9d67cca07edb8eb0d676f8e2c42394c
uxcandy_boomyjee <debian5-64.test>
2014-02-05 12:16:20 +0100

    FIX: keypress emulation to run only once

--- plugins/node_runner/nodeapp/server.js
+++ plugins/node_runner/nodeapp/server.js
@@ -2,8 +2,7 @@ var http = require("http");
 
 http.createServer(function(request, response) {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello World");
-    // console.log("started");
-    //bla();
+    response.write("Hello World Live");
     response.end();
+    console.log("some message\n");
 }).listen(8888);
