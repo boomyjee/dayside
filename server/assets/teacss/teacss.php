b41aa796b6f2b74c58606fabf2eb750900c40b5c
uxcandy_boomyjee <debian5-64.test>
2014-02-07 20:53:55 +0100

    Build function for making tea files with authentification

---
+++ server/assets/teacss/teacss.php
@@ -0,0 +1,42 @@
+<?
+
+function teacss($makefile,$css,$js,$dir,$dev) {
+    if ($dev) {
+        if (isset($_REQUEST['remote'])) {
+            ob_get_clean();
+            echo file_get_contents($_REQUEST['remote']); 
+            die();
+        }
+        if (isset($_POST['css'])) {
+            ob_get_clean();
+            if ($css) file_put_contents(realpath($dir)."/".basename($js),$_POST['js']);
+            if ($js) file_put_contents(realpath($dir)."/".basename($css),$_POST['css']);
+            echo 'ok';
+            die();
+        }   
+        
+        echo ob_get_clean();
+        
+        ?>
+            <script tea="<?=$makefile?>"></script>
+            <script>teacss.update()</script>
+            <script>
+                teacss.buildCallback = function (files) {
+                    teacss.jQuery.post(location.href,{css:files['/default.css'],js:files['/default.js']},function(data){
+                        alert(data);
+                    });
+                }
+            </script>
+        <?
+        return 'dev';
+    } else {
+        echo ob_get_clean();
+        ?>
+            <? if ($js): ?><script src="<?=$js?>"></script><? endif ?>
+            <? if ($css): ?><link href="<?=$css?>" rel="stylesheet" type="text/css"><? endif ?>
+        <?
+        return 'release';
+    }
+}
+
+ob_start();
