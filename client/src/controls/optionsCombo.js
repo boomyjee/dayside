8aea008dfe21add038327c314032601a7aa980a2
boomyjee <boomyjee@gmail.com>
2014-03-11 14:27:42 +0100

    11_03_2013

--- client/src/controls/optionsCombo.js
+++ client/src/controls/optionsCombo.js
@@ -24,24 +24,26 @@ ui.optionsCombo = teacss.ui.Combo.extend({
         $.each(themes,function(t,theme){ themeOptions[theme] = theme; });
         
         this.form = ui.form(function(){
-            panel = ui.panel({width:200,height:'auto',margin:0,padding:"5px 10px 10px"}).push(
-                ui.label({template:'Font size: ${value}px',name:'fontSize',margin:"0"}),
+            panel = ui.panel({width:"100%",height:'auto',margin:0,padding:"5px 10px 10px"}).push(
+                ui.label({template:'Font size: ${value}px',name:'fontSize',margin:"5px 0"}),
                 ui.slider({min:10,max:24,margin:"0px 15px 0px",name:'fontSize'}),
-                ui.label({template:'Tab size: ${value}',name:'tabSize',margin:"0 0 0 0"}),
+                ui.label({template:'Tab size: ${value}',name:'tabSize',margin:"5px 0"}),
                 ui.slider({min:1,max:16,margin:"0px 15px 0px",name:'tabSize'}),
                 check = ui.check({margin:"10px 15px 5px 15px",width:'100%',label:'Use tab character',name:'useTab'}),
-                ui.label({template:'Theme:',margin:"0 0 0 0"}),
+                ui.label({template:'Theme:',margin:"5px 0"}),
                 ui.select({name: 'theme', items: themeOptions,width:"100%", comboDirection: 'right', comboHeight: 1000, margin: "-3px 0 0 0" })
             );
         });
         check.element.css("font-size",10);
-        this._super($.extend(options,{items:[panel]}));
+        this._super($.extend({comboWidth:200},options,{items:[panel]}));
         this.loadValue();
         this.form.bind("change",function(){
             me.value = me.form.value;
+            me.updateOptions();
             me.trigger("change");
             me.saveValue();
         });
+        this.updateOptions();
     },
     saveValue: function () {
         dayside.storage.set("options",this.value);
@@ -49,7 +51,39 @@ ui.optionsCombo = teacss.ui.Combo.extend({
     loadValue: function () {
         this.value = $.extend({},this.defaults,dayside.storage.get("options",{}));
         this.form.setValue(this.value);
-    }
+    },
+    updateOptions: function () {
+        var ui = teacss.ui;
+        var value = this.value;
+
+        var theme = value.theme || 'default';
+        $("body").attr("class","cm-s-"+theme);
+
+        // apply indent settings to CodeMirror defaults and opened editors
+        CodeMirror.defaults.tabSize = value.tabSize;
+        CodeMirror.defaults.indentUnit = value.tabSize;
+        CodeMirror.defaults.indentWithTabs = value.useTab;
+
+        for (var t=0;t<ui.codeTab.tabs.length;t++) {
+            var e = ui.codeTab.tabs[t].editor;
+            if (e) {
+                e.setOption("tabSize",value.tabSize);
+                e.setOption("indentUnit",value.tabSize);
+                e.setOption("indentWithTabs",value.useTab);
+            }
+        }             
+
+        // create dynamic CSS node to reflect fontSize changes for CodeMirror
+        var styles = $("#ideStyles");
+        if (styles.length==0) {
+            styles = $("<style>").attr({type:"text/css",id:"ideStyles"}).appendTo("head");
+        }
+        styles.html(".CodeMirror {font-size:"+value.fontSize+"px !important; }");
+        for (var t=0;t<ui.codeTab.tabs.length;t++) {
+            var e = ui.codeTab.tabs[t].editor;
+            if (e) e.refresh();
+        }            
+    }  
 });
     
 })(teacss.jQuery,teacss.ui);
