9a257d0566190ae1921500ef6c7b1affd2778994
uxcandy_maxtun <uxcandy_maxtun@vdc4892.de>
2015-03-06 14:56:23 +0400

    'ok'

--- server/demo.php
+++ server/demo.php
@@ -3,6 +3,7 @@ if (!isset($_REQUEST['_type'])) return;
     
 require "api.php";
 require __DIR__."/../plugins/collaborate/api.php";
+require __DIR__."/../plugins/git_commit/api.php";
 
 class DemoApi extends FileApi {
     static $firebase_secret = "";
