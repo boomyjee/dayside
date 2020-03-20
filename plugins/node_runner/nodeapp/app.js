1cbcdc49ea7daea04c6dc9eee1266404a73b8bca
boomyjee <boomyjee@gmail.com>
2020-03-20 05:23:32 +0100

    'remove node runner plugin'

--- plugins/node_runner/nodeapp/app.js
+++
@@ -1,8 +0,0 @@
-var http = require("http");
-
-http.createServer(function(request, response) {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello World Live");
-    response.end();
-    console.log("some message\n");
-}).listen(process.env.PORT);
